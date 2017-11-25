557b5937ab2cf9c05a66ad32fdf510abb044e1f4
Bai Jie <baijie1991@gmail.com>
2017-11-26 06:43:30 +0800

    init with Spring Initializr
    Gradle + Kotlin + Spring Boot 2.0.0 M6 + Reactive Web

---
+++ src/main/kotlin/gq/baijie/speedtest/SpeedtestApplication.kt
@@ -0,0 +1,11 @@
+package gq.baijie.speedtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpeedtestApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpeedtestApplication>(*args)
+}
