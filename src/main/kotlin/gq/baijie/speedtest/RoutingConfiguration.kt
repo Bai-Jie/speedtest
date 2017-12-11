d298830efbaf79ca79240a5d6fcd6ad3a8da9984
Bai Jie <baijie1991@gmail.com>
2017-12-12 01:04:27 +0800

    convert RoutingConfiguration to Kotlin code
    use Kotlin routing DSL
    
    see https://docs.spring.io/spring/docs/5.0.2.RELEASE/spring-framework-reference/languages.html#webflux-functional-dsl
    see https://docs.spring.io/spring-framework/docs/5.0.2.RELEASE/kdoc-api/spring-framework/org.springframework.web.reactive.function.server/-router-function-dsl/

---
+++ src/main/kotlin/gq/baijie/speedtest/RoutingConfiguration.kt
@@ -0,0 +1,40 @@
+package gq.baijie.speedtest
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
+
+@Configuration
+class RoutingConfiguration {
+
+    @Bean
+    fun echoRouterFunction(): RouterFunction<ServerResponse> {
+//        return route(GET("/echo").or(POST("/echo")), HandlerFunction<ServerResponse> { echo(it) })
+        return router {
+//            GET("/echo", ::echo)
+//            POST("/echo", ::echo)
+//            (GET("/echo") or POST("/echo"))(::echo)
+//            (GET("/echo") or POST("/echo"))(::echo)
+//            ((method(HttpMethod.GET) or method(HttpMethod.POST)) and "/echo")(::echo)
+            val GET = method(HttpMethod.GET)
+            val POST = method(HttpMethod.POST)
+            ((GET or POST) and "/echo")(::echo)
+        }
+    }
+
+    @Bean
+    fun usersRouterFunction(userHandler: UserHandler): RouterFunction<ServerResponse> {
+        return router {
+            ("/users" and accept(APPLICATION_JSON)).nest {
+                GET("/{user}", userHandler::getUser)
+                GET("/{user}/customers", userHandler::getUserCustomers)
+                DELETE("/{user}", userHandler::deleteUser)
+            }
+        }
+    }
+
+}
