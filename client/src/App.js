03c84bf8a32595bd4bb7405b8f33090341a1bd80
Bai Jie <baijie1991@gmail.com>
2018-01-10 22:32:18 +0800

    use material-ui React component set
    see https://material-ui-next.com/

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import 'typeface-roboto'
+import {createMuiTheme, MuiThemeProvider} from "material-ui";
 import {SpeedTestLinkList} from "./SpeedTestLinkList";
+
+const theme = createMuiTheme({
+  typography: {
+    // default fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif'
+    fontFamily: '"Roboto", "Noto Sans", "Noto Sans CJK SC", "Helvetica", "Arial", sans-serif'
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider theme={theme}>
         <SpeedTestLinkList/>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
